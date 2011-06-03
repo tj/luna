d7399b023f049cf5e172d9adc00dba472a10cdf5
Tj Holowaychuk <tj@vision-media.ca>
2011-06-02 21:11:27 -0700

    colors for luna_token_inspect()

--- src/token.c
+++ src/token.c
@@ -17,19 +17,19 @@ void
 luna_token_inspect(luna_token_t *tok) {
   switch (tok->type) {
     case LUNA_TOKEN_INT:
-      printf("integer %d\n", tok->value.as_int);
+      printf("\033[90minteger\033[0m \033[36m%d\033[0m\n", tok->value.as_int);
       break;
     case LUNA_TOKEN_FLOAT:
-      printf("float %f\n", tok->value.as_float);
+      printf("\033[90mfloat\033[0m \033[36m%f\033[0m\n", tok->value.as_float);
       break;
     case LUNA_TOKEN_STRING:
-      printf("string '%s'\n", tok->value.as_string);
+      printf("\033[90mstring\033[0m \033[32m'%s'\033[0m\n", tok->value.as_string);
       break;
     case LUNA_TOKEN_ID:
-      printf("identifier %s\n", tok->value.as_string);
+      printf("\033[90midentifier\033[0m %s\n", tok->value.as_string);
       break;
     default:
-      printf("%s\n", luna_token_type_string(tok->type));
+      printf("\033[90m%s\033[0m\n", luna_token_type_string(tok->type));
       break;
   }
 }
