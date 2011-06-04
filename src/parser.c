c758f7aab7fad333205f8be0abdc74f7022cc039
Tj Holowaychuk <tj@vision-media.ca>
2011-06-03 19:14:14 -0700

    docs

--- src/parser.c
+++ src/parser.c
@@ -8,10 +8,18 @@
 #include <stdio.h>
 #include "parser.h"
 
+/*
+ * Initialize with the given lexer.
+ */
+
 void
 luna_parser_init(luna_parser_t *self, luna_lexer_t *lex) {
   self->lex = lex;
 }
+
+/*
+ * Parse input.
+ */
 
 void
 luna_parse(luna_parser_t *self) {
