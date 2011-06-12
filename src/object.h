9dc1ea9d17515b66f44aa314f2ec226303a4e082
Tj Holowaychuk <tj@vision-media.ca>
2011-06-12 12:59:45 -0700

    Added sr/object.h

---
+++ src/object.h
@@ -0,0 +1,35 @@
+
+//
+// object.h
+//
+// Copyright (c) 2011 TJ Holowaychuk <tj@vision-media.ca>
+//
+
+#ifndef __LUNA_OBJECT_H__
+#define __LUNA_OBJECT_H__
+
+/*
+ * Luna object types.
+ */
+
+typedef enum {
+    LUNA_TYPE_INT
+  , LUNA_TYPE_FLOAT
+  , LUNA_TYPE_STRING
+  , LUNA_TYPE_OBJECT
+} luna_object;
+
+/*
+ * Luna object.
+ */
+
+typedef struct {
+  luna_object type;
+  union {
+    void *as_pointer;
+    int as_int;
+    float as_float;
+  } val;
+} luna_object_t;
+
+#endif /* __LUNA_OBJECT_H__ */
