6ecfa3bde7a3959e4e50d39ba60cc50b09cfddc4
Tj Holowaychuk <tj@vision-media.ca>
2011-07-12 17:47:58 -0700

    stubbed out rewriter

---
+++ src/rewriter.c
@@ -0,0 +1,20 @@
+
+//
+// rewriter.c
+//
+// Copyright (c) 2011 TJ Holowaychuk <tj@vision-media.ca>
+//
+
+#include <stdio.h>
+#include "ast.h"
+#include "array.h"
+#include "rewriter.h"
+
+/*
+ * Optimize the AST, starting by visiting `node`.
+ */
+
+void
+luna_rewrite(luna_node_t *node) {
+  
+}
---
+++ src/rewriter.h
@@ -0,0 +1,16 @@
+
+//
+// rewriter.h
+//
+// Copyright (c) 2011 TJ Holowaychuk <tj@vision-media.ca>
+//
+
+#ifndef __LUNA_REWRITER__
+#define __LUNA_REWRITER__
+
+#include "ast.h"
+
+void
+luna_rewrite(luna_node_t *node);
+
+#endif /* __LUNA_REWRITER__ */
