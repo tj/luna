7c20ad0bf92c97b18d9a98df393e79f0cfe922ae
Tj Holowaychuk <tj@vision-media.ca>
2011-07-12 18:06:34 -0700

    implemented luna_visit()

---
+++ src/visitor.c
@@ -0,0 +1,50 @@
+
+//
+// visitor.c
+//
+// Copyright (c) 2011 TJ Holowaychuk <tj@vision-media.ca>
+//
+
+#include "visitor.h"
+
+void
+luna_visit(luna_visitor_t *self, luna_node_t *node) {
+  switch (node->type) {
+    case LUNA_NODE_BLOCK:
+      self->visit_block((luna_block_node_t *) node, self);
+      break;
+    case LUNA_NODE_ID:
+      self->visit_id((luna_id_node_t *) node, self);
+      break;
+    case LUNA_NODE_INT:
+      self->visit_int((luna_int_node_t *) node, self);
+      break;
+    case LUNA_NODE_FLOAT:
+      self->visit_float((luna_float_node_t *) node, self);
+      break;
+    case LUNA_NODE_STRING:
+      self->visit_string((luna_string_node_t *) node, self);
+      break;
+    case LUNA_NODE_SLOT:
+      self->visit_slot((luna_slot_node_t *) node, self);
+      break;
+    case LUNA_NODE_CALL:
+      self->visit_call((luna_call_node_t *) node, self);
+      break;
+    case LUNA_NODE_IF:
+      self->visit_if((luna_if_node_t *) node, self);
+      break;
+    case LUNA_NODE_WHILE:
+      self->visit_while((luna_while_node_t *) node, self);
+      break;
+    case LUNA_NODE_UNARY_OP:
+      self->visit_unary_op((luna_unary_op_node_t *) node, self);
+      break;
+    case LUNA_NODE_BINARY_OP:
+      self->visit_binary_op((luna_binary_op_node_t *) node, self);
+      break;
+    case LUNA_NODE_FUNCTION:
+      self->visit_function((luna_function_node_t *) node, self);
+      break;
+  }
+}
--- src/visitor.h
+++ src/visitor.h
@@ -8,23 +8,28 @@
 #ifndef __LUNA_VISITOR__
 #define __LUNA_VISITOR__
 
+#include "ast.h"
+
 /*
  * Visitor struct.
  */
 
-typedef struct {
-  void (* visit_block)(luna_block_node_t *);
-  void (* visit_id)(luna_id_node_t *);
-  void (* visit_int)(luna_int_node_t *);
-  void (* visit_float)(luna_float_node_t *);
-  void (* visit_string)(luna_string_node_t *);
-  void (* visit_slot)(luna_slot_node_t *);
-  void (* visit_call)(luna_call_node_t *);
-  void (* visit_while)(luna_while_node_t *);
-  void (* visit_unary_op)(luna_unary_op_node_t *);
-  void (* visit_binary_op)(luna_binary_op_node_t *);
-  void (* visit_function)(luna_function_node_t *);
-  void (* visit_if)(luna_if_node_t *);
+typedef struct luna_visitor {
+  void (* visit_block)(luna_block_node_t *node, struct luna_visitor *self);
+  void (* visit_id)(luna_id_node_t *node, struct luna_visitor *self);
+  void (* visit_int)(luna_int_node_t *node, struct luna_visitor *self);
+  void (* visit_float)(luna_float_node_t *node, struct luna_visitor *self);
+  void (* visit_string)(luna_string_node_t *node, struct luna_visitor *self);
+  void (* visit_slot)(luna_slot_node_t *node, struct luna_visitor *self);
+  void (* visit_call)(luna_call_node_t *node, struct luna_visitor *self);
+  void (* visit_while)(luna_while_node_t *node, struct luna_visitor *self);
+  void (* visit_unary_op)(luna_unary_op_node_t *node, struct luna_visitor *self);
+  void (* visit_binary_op)(luna_binary_op_node_t *node, struct luna_visitor *self);
+  void (* visit_function)(luna_function_node_t *node, struct luna_visitor *self);
+  void (* visit_if)(luna_if_node_t *node, struct luna_visitor *self);
 } luna_visitor_t;
+
+void
+luna_visit(luna_visitor_t *self, luna_node_t *node);
 
 #endif /* __LUNA_VISITOR__ */
